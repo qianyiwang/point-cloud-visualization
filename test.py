c31a8bff3bb7ac0f37acc260b019697371c7a385
Wang <QWANG97@ford.com>
2019-01-09 23:13:03 +0800

    add animate

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from velodyne_pointcloud_viz import VelodynePointsViz
 
 velo_viz = VelodynePointsViz('../self-driving-viz/kitti_data/2011_09_26/2011_09_26_drive_0001_sync/velodyne_points/data')
-velo_viz.plot_velo_frame_mayai('0000000000.bin')
+# velo_viz.plot_velo_frame_mayai('0000000000.bin')
 # velo_viz.plot_velo_frame_matplotlib('0000000000.bin')
+velo_viz.kitti_velo_animat('0000000000.bin')
--- velodyne_pointcloud_viz.py
+++ velodyne_pointcloud_viz.py
@@ -1,24 +1,23 @@
 
 import os
 import numpy as np
+import mayavi.mlab
 class VelodynePointsViz():
 	"""docstring for VelodynePointsViz"""
 
 	def __init__(self, path):
 		self.velo_path = path
 		self.points = []
+		self.map = None
 
 	def __load_kitti_velo_points(self, file_name, chunksize=4): # this is a private function
 
 		path = os.path.join(self.velo_path, file_name)
-		print(path)
 		self.points = np.fromfile(path, dtype=np.float32).reshape(-1, chunksize)
 		
 	def plot_velo_frame_mayai(self, file_name, points = 0.2):
-    
-	    import mayavi.mlab
 
-	    if len(self.points) == 0:
+	    if len(self.points) == 0 and file_name != None:
 	    	self.__load_kitti_velo_points(file_name)
 
 	    fig = mayavi.mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
@@ -37,11 +36,11 @@ class VelodynePointsViz():
 	    # velo[:, 3], # reflectance values
 	    mayavi.mlab.show()
 
-	def plot_velo_frame_matplotlib(self, file_name):
+	def plot_velo_frame_matplotlib(self, file_name=None):
 	    import matplotlib.pyplot as plt
 	    from mpl_toolkits.mplot3d import Axes3D
 
-	    if len(self.points) == 0:
+	    if len(self.points) == 0 and file_name != None:
 	    	self.__load_kitti_velo_points(file_name)
 
 	    skip = 100   # Skip every n points
@@ -57,3 +56,29 @@ class VelodynePointsViz():
 	               marker="o")
 	    ax.axis('scaled')  # {equal, scaled}
 	    plt.show()
+
+	@mayavi.mlab.animate(delay = 200)
+	def __update_animation(self):
+		for f in os.listdir(self.velo_path):
+			self.map.mlab_source.set(self.__load_kitti_velo_points(f))
+			yield
+
+	def kitti_velo_animat(self, start_file):
+		self.__load_kitti_velo_points(start_file)
+		fig = mayavi.mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
+		self.map = mayavi.mlab.points3d(
+			self.points[:, 0],   # x
+			self.points[:, 1],   # y
+			self.points[:, 2],   # z
+			self.points[:, 2],   # Height data used for shading
+			mode="point", # How to render each point {'point', 'sphere' , 'cube' }
+			colormap='spectral',  # 'bone', 'copper',
+			#color=(0, 1, 0),     # Used a fixed (r,g,b) color instead of colormap
+			scale_factor=100,     # scale of the points
+			line_width=10,        # Scale of the line, if any
+			figure=fig,
+		)
+		self.__update_animation()
+		mayavi.mlab.show()
+
+	    
